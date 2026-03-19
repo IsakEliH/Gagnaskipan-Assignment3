8f1e06ccc936bd18ae5eeff95aec9231bc372495
IsakEliH <isakelihauksson@gmail.com>
2026-03-19 12:03:31 +0000

    feat: word_freq.py is completed

--- word_freq.py
+++ word_freq.py
@@ -6,12 +6,14 @@
 
 from my_dict import MyDict
 
+
 def read_in_text(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         text = file.read()
         return text
 
-def word_frequency_alphabetical_pydict(text):
+
+def word_frequency_alphabetical_pydict(text: str) -> list[tuple[str, int]]:
     """
     Returns a list of (word, frequency) tuples ordered alphabetically, with all words translated to lowercase,
     e.g., given the text "I am so so happy happy Happy" it returns [('am', 1), ('happy', 3), ('i', 1), ('so', 2)].
@@ -19,8 +21,18 @@ def word_frequency_alphabetical_pydict(text):
     :param text: text to process
     :return: list of word frequencies
     """
-    # TO DO ...
-    return []
+    
+    text_list: list[str] = text.lower().split()
+    word_frequency: dict[str, int] = {}
+
+    for word in text_list:
+        try:
+            word_frequency[word] += 1
+        except KeyError:
+            word_frequency[word] = 1
+
+    return [(key,word_frequency[key]) for key in word_frequency]
+
 
 def word_frequency_alphabetical_mydict(text):
     """
@@ -30,12 +42,20 @@ def word_frequency_alphabetical_mydict(text):
     :param text: text to process
     :return: list of word frequencies
     """
-    # TO DO ...
-    return []
+    text_list: list[str] = text.lower().split()
+    word_frequency: MyDict = MyDict()
+
+    for word in text_list:
+        try:
+            word_frequency[word] += 1  # type: ignore
+        except KeyError:
+            word_frequency[word] = 1
+
+    return [(key,word_frequency[key]) for key in word_frequency]
 
 
 if __name__ == "__main__":
-    text = read_in_text('data/word1.txt')
+    text = read_in_text("data/word1.txt")
     l_py = word_frequency_alphabetical_pydict(text)
     print(l_py)
     l_my = word_frequency_alphabetical_mydict(text)
